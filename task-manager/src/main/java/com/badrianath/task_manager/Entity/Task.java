c5d669aaea0fb798acfc85a714199007feb0aa03
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-05-15 08:30:45 +0530

    start a task manager project

---
+++ task-manager/src/main/java/com/badrianath/task_manager/Entity/Task.java
@@ -0,0 +1,18 @@
+package com.badrianath.task_manager.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean completed;
+    private LocalDateTime createdAt;
+}
